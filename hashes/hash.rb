092316b84bb1a754af166e46a49957cc4dcfe444
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-25 22:13:12 -0300

    Hashes

---
+++ hashes/hash.rb
@@ -0,0 +1,17 @@
+hash = {nome: "Diego", idade: 35}
+hash[:altura] = 1.73
+#hash.delete(:altura) # Como deletar um hash
+
+#puts hash.keys
+#puts hash.values
+
+# puts hash.size Ou ainda
+puts hash.length
+
+#puts hash.has_key?(:nome_completo)
+#puts hash.clear # Limpa todo o hash
+#puts hash[:nome]
+
+#hash1 = { :um => 1, :dois => 2, :tres => 3 }
+#hash2 = { :tres => 3, :quatro => 4, :cinco => 5}
+#puts hash1 == hash2
