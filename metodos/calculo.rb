0c8690ac03e83573b758aa570df4733e86819938
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-26 21:53:43 -0300

    Metodos

---
+++ metodos/calculo.rb
@@ -0,0 +1,20 @@
+def soma(x, y)
+  x + y
+end
+
+def subt(x,y)
+  x - y
+end
+
+def mult(x,y)
+  x * y
+end
+
+def div(x,y)
+  x / y
+end
+
+puts soma(3,4)
+puts subt(3,4)
+puts mult(3,4)
+puts div(3,4)
