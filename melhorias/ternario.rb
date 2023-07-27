730cd48d298e276d30c2cfea37929d46b7f1063a
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-26 21:59:55 -0300

    Operador Ternario

---
+++ melhorias/ternario.rb
@@ -0,0 +1,12 @@
+nome = "Diego"
+
+if nome.eql?("Lucas")
+  puts "Dono do canal Hardware e Afins"
+else
+  puts "Visitante"
+end
+
+# Sintaxe do operador ternário
+# condição ? verdadeiro : falso
+
+puts nome.eql?("Diego") ? "Dono do canal Noob Code" : "Visitante"
